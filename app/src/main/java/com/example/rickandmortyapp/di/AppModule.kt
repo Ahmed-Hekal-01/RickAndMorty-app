a06b9e4607aace7cf5dad2fce619bbbee094406a
Ahemd-Hekal-01 <ahmedsanta021@gmail.com>
2026-04-07 22:17:28 +0200

    feat: added the di for providing the repos

--- app/src/main/java/com/example/rickandmortyapp/di/AppModule.kt
+++ app/src/main/java/com/example/rickandmortyapp/di/AppModule.kt
@@ -2,6 +2,12 @@ package com.example.rickandmortyapp.di
 import com.example.rickandmortyapp.data.api.KtorClient
 import com.example.rickandmortyapp.data.api.service.IRickAndMortyApiService
 import com.example.rickandmortyapp.data.api.service.KtorRickAndMortyService
+import com.example.rickandmortyapp.data.repository.CharacterRepository
+import com.example.rickandmortyapp.data.repository.EpisodeRepository
+import com.example.rickandmortyapp.data.repository.ICharacterRepository
+import com.example.rickandmortyapp.data.repository.IEpisodeRepository
+import com.example.rickandmortyapp.data.repository.ILocationRepository
+import com.example.rickandmortyapp.data.repository.LocationRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,4 +28,22 @@ object AppModule {
     fun provideRickAndMortyApiService(
         client: HttpClient
     ): IRickAndMortyApiService = KtorRickAndMortyService(client)
+
+    @Provides
+    @Singleton
+    fun provideCharacterRepository(
+        apiService: IRickAndMortyApiService
+    ): ICharacterRepository = CharacterRepository(apiService)
+
+    @Provides
+    @Singleton
+    fun provideLocationRepository(
+        apiService: IRickAndMortyApiService
+    ): ILocationRepository = LocationRepository(apiService)
+
+    @Provides
+    @Singleton
+    fun provideEpisodeRepository(
+        apiService: IRickAndMortyApiService
+    ): IEpisodeRepository = EpisodeRepository(apiService)
 }
