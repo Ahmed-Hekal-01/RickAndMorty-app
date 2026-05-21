41359bee9197def379ecd90cc3d51e480ae7d63a
Ahemd-Hekal-01 <ahmedsanta021@gmail.com>
2026-05-22 00:15:21 +0300

    chore: added the navigation deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
 
     // Hilt Navigation Compose
     implementation(libs.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
